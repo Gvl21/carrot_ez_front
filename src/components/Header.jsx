a625e22e335410b232c1afbbb3df06cd3bf18d66
songyi <songyee112@naver.com>
2023-11-13 11:40:46 +0900

    header 틀 작성 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <h1>당근이지🥕</h1>
+        <h3>친구찾기</h3>
+        <h3>글쓰기</h3>
+        <h3>로그인</h3>
+        <h3>회원가입</h3>
+    </div>
   )
 }
 
