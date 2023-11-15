03c91eb6db1dfa1bec0e260b21d7ff856a34ec56
songyi <songyee112@naver.com>
2023-11-15 10:35:39 +0900

    지역 드롭다운 생성 완료

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -51,17 +51,20 @@ function Member() {
                     <label>지역:</label>
                     <select name='area' value={formData.area} onChange={handleChange}>
                         <option value=''>선택하세요</option>
-                        <option value='seoul'>서울</option>
-                        <option value='busan'>부산</option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-                        <option value=''></option>
-
+                        <option value='seoul'>서울특별시</option>
+                        <option value='incheon'>인천광역시</option>
+                        <option value='gyeongi'>경기도</option>
+                        <option value='gangwon'>강원도</option>
+                        <option value='chungcheong'>충청도</option>
+                        <option value='sejong'>세종특별시</option>
+                        <option value='daejeon'>대전광역시</option>
+                        <option value='jeonra'>전라도</option>
+                        <option value='gwangju'>광주광역시</option>
+                        <option value='daegu'>대구광역시</option>
+                        <option value='ulsan'>울산광역시</option>
+                        <option value='gyeongsang'>경상도</option>
+                        <option value='busan'>부산광역시</option>
+                        <option value='jeju'>제주특별시</option>
                     </select>
 
                    
