cf21eb0123a00613d22af23565331060dc0b7f01
songyi <songyee112@naver.com>
2023-11-16 16:16:16 +0900

    메인에 return태그 추가

--- src/pages/FindFriend.jsx
+++ src/pages/FindFriend.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function FindFriend() {
   return (
-    <div>FindFriend</div>
+    <div>
+      <h1> 친구를 찾아봅쉬다!! </h1>
+    </div>
   )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 function Main() {
+    return (
     <div>
-       main
+       <h1>Main</h1>
     </div>
+    )
 }
 
 export default Main;
