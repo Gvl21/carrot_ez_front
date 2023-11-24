7c1bd416c195f952a094f8698bf4e1fb4a9ce00f
songyi <songyee112@naver.com>
2023-11-24 12:21:35 +0900

    메인 상단 more에 css요소 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,13 +3,25 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import WriteList from './WriteList';
 import './Main.css'
 
+
+
 function Main() {
+
+    const navigate = useNavigate();
+
+
+    const goFindFriend = () => {
+        navigate('/findfriend');
+    }
+
     return (
         <div className='main'>
             <div className='fstvlapi'>
                 <h1>축제 API자리</h1>
             </div>
+
             <div className='writelist'>
+                <h2 className='more' onClick={goFindFriend}>more👉</h2>
             <WriteList />
             </div>
         
