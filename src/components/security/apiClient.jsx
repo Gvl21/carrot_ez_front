f28c65a5c0ea47422185b33bbbf01cba3b2546a0
Gvl21 <thornyk1@gmail.com>
2023-11-28 02:20:18 +0900

    apiClient ip기반으로 재변경

--- src/components/security/apiClient.jsx
+++ src/components/security/apiClient.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // API 요청을 할 Clinet 생성 및 응답해줄 BackEnd url 정의
 export const apiClient = axios.create({
-    // baseURL: 'http://10.100.203.39',
-    baseURL: 'http://localhost',
+    baseURL: 'http://10.100.203.39',
+    // baseURL: 'http://localhost',
 });
 
 export const signInApi = async (formData) => {
