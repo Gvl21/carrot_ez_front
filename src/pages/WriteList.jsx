1bd1149b6fef06b740800f5e5ba7c6fe3e54bcd8
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-11-28 09:47:38 +0900

    다시 수정중

--- src/pages/WriteList.jsx
+++ src/pages/WriteList.jsx
@@ -5,61 +5,72 @@ import './WriteList.css';
 import { SearchProvider } from '../components/Search';
 import SearchBar from '../components/SearchBar';
 import { getArticleList } from '../components/security/apiClient';
-
 const WriteList = () => {
-const initialData = [
-  {
-    id: '1',
-    area: '부산광역시',
-    category: '운동',
-    date: '2023-11-19',
-    nickname: '형수',
-    title: '광안리에서 산책하실분~',
-    content: '광안리에서 산책 하실분을 모집합니다',
-  },
-  {
-    id: '2',
-    area: '부산광역시',
-    category: '문화생활',
-    date: '2023-11-20',
-    nickname: '가은',
-    title: '원데이클래스 같이하실분~',
-    content: '기타 같이 배워볼 분 구합니다.',
-  },
-  {
-    id: '3',
-    area: '인천광역시',
-    category: '게임',
-    date: '2023-11-21',
-    nickname: '송이',
-    title: '지역상관없이 옵치 그룹 구합니다~',
-    content: '티어는 골드구간입니다 같이 플레가실분 구합니다',
-  },
-];
-  const [posts, setPosts] = useState(initialData);
-
+    const initialData = [
+        {
+            id: '1',
+            area: '부산광역시',
+            category: '운동',
+            date: '2023-11-19',
+            nickname: '형수',
+            title: '광안리에서 산책하실분~',
+            content: '광안리에서 산책 하실분을 모집합니다',
+        },
+        {
+            id: '2',
+            area: '서울특별시',
+            category: '문화생활',
+            date: '2023-11-20',
+            nickname: '가은',
+            title: '원데이클래스 같이하실분~',
+            content: '성수에서 원데이클래스 같이 하실분~ 선착순 한명',
+        },
+        {
+            id: '3',
+            area: '인천광역시',
+            category: '게임',
+            date: '2023-11-21',
+            nickname: '송이',
+            title: '지역상관없이 옵치 그룹 구합니다~',
+            content: '티어는 골드구간입니다 같이 플레가실분 구합니다',
+        },
+        // { id: '4',
+        //   area: '제주특별시',
+        //   category: '게임',
+        //   date: '2023-11-27',
+        //   nickname: '얀또니',
+        //   title : '같이 크아 하실분',
+        //   content: '같이 크레이지아케이드 하실분~',
+        // },
+        // { id: '5',
+        // area: '대전광역시',
+        // category: '운동',
+        // date: '2023-11-28',
+        // nickname: '또잉이',
+        // title : '같이 헬스장 다니실분',
+        // content: '일주일에 세번 날짜 정해서 같이 하실 분 구합니다',
+        // },
+    ];
+    const [posts, setPosts] = useState(initialData);
+    const [filteredPosts, setFilteredPosts] = useState([]);
     const handleSearch = (searchTerm) => {
         const filtered = posts.filter((post) =>
             post.title.includes(searchTerm)
         );
         setFilteredPosts(filtered);
     };
-
     const showArticles = async () => {
         const responseBody = await getArticleList();
         const articleList = responseBody.articleList;
         console.log(articleList);
         setPosts(articleList);
     };
-
     useEffect(() => {
         showArticles();
     }, []);
-
     return (
         <div className='find-friend-container'>
             <SearchBar onSearch={handleSearch} />
-
             <ul className='post-list'>
                 {filteredPosts.length > 0
                     ? filteredPosts.map((post) => (
@@ -116,5 +127,11 @@ const initialData = [
         </div>
     );
 };
-
 export default WriteList;
+
+
+
+
+
+
+
