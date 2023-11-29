fdd07202440de26da5acf4063fff77d9077ca967
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-11-29 14:32:48 +0900

    글쓰기 수정 및 삭제하는 로직 만드는 중

--- src/App.js
+++ src/App.js
@@ -13,8 +13,41 @@ import { AuthProvider, useAuth } from './components/security/AuthContext';
 import { apiClient } from './components/security/apiClient';
 import FindFriend from './pages/FindFriend';
 
-export const StateContext = React.createContext();
-export const ImagesContext = React.createContext();
+function reducer(state, action) {
+    switch (action.type) {
+      case 'INIT' : {
+        // localStorage에서 가져온 데이터로 상태를 변경
+        return action.data;
+      }
+      case 'CREATE' : {
+        // 데이터 배열에 새 데이터를 추가 (spread 연산자)
+        const newState = [action.data, ...state];
+        localStorage.setItem('diaryList', JSON.stringify(newState));
+        return newState;
+                // 새로운 데이터를   ...기존의 데이터에 추가
+      }
+      case 'UPDATE' : {
+        // 기존 데이터 배열에서 id에 매칭되는 내용 수정 (삼항연산자 사용)
+        const newState =  state.map((item)=> 
+        String(item.id) === String(action.data.id) ?  { ...action.data } : item);
+        localStorage.setItem('diaryList', JSON.stringify(newState));
+        return newState;
+        // (item) => (조건식) ? (참일 경우, id가 일치할경우) : item 
+      }
+      case 'DELETE' : {
+        // 기존 데이터에서 id가 일치하지 않는 데이터만 남김 (filter)
+        const newState = state.filter((item) => 
+        String(item.id) !== String(action.id));
+        localStorage.setItem('diaryList', JSON.stringify(newState));
+        return newState;
+      }
+        default : {
+        return state
+        }
+      }
+    }
+    export const StateContext = React.createContext();
+    export const DispatchContext = React.createContext();
 
 function App() {
     // 이미지 컨텍스트용 상태값
@@ -79,6 +112,8 @@ function App() {
         getSignInUserInfo(cookies.accessToken);
         setIsLoggedIn(true);
     }, [cookies.accessToken]);
+
+   
 
     return (
         <div className='App'>
---
+++ src/components/Editor.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { Dispatcher } from 'undici-types'
+
+function Editor() {
+    const{onCreate, onUpdate} = useContext(DispatchContext)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Editor
