d46859fe351b3a90039d62fdbf0bc1d8b426346f
songyi <songyee112@naver.com>
2023-11-29 12:24:48 +0900

    detail page CSS 수정

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getArticleDetails } from '../components/security/apiClient'; // 상세 정보를 가져오는 함수를 import
+import { getArticleDetails } from '../components/security/apiClient'; 
+import './DetailPage.css'
 
 const DetailPage = () => {
     const { id } = useParams(); // URL에서 파라미터 추출
@@ -28,12 +29,23 @@ const DetailPage = () => {
 
     return (
         <div className='post-details-container'>
+            <div className='detail-title'>
             <h2>{postDetails.title}</h2>
+            </div>
+            <div className='detail-category'>
             <p>지역: {postDetails.area}</p>
             <p>카테고리: {postDetails.category}</p>
+            </div>
+            <div className='detail-date'>
             <p>작성일: {postDetails.regTime}</p>
-            <p>작성자: {postDetails.nickname}</p>
+            </div>
+            <div className='detail-nickname'>
             <img src={postDetails.memberImageUrl} alt='프로필' />
+            <p>작성자: {postDetails.nickname}</p>
+            </div>
+
+            <hr/>
+            
             {/* 게시글 업로드 이미지 넣을 곳  */}
             <p>{postDetails.content}</p>
         </div>
