6b0348a5e2554733005e0586e857d0a3b13c83a9
songyi <songyee112@naver.com>
2023-11-29 12:11:12 +0900

    제목 slice 및 프로필사진 - 작성자 순서 수정

--- src/components/MainWriteList.jsx
+++ src/components/MainWriteList.jsx
@@ -59,7 +59,7 @@ const MainWriteList = () => {
                             to={`/detail/${post.articleId}`}
                             className='post-title'
                         >
-                            {post.title}
+                            {post.title.slice(0,10)+'...'}
                         </Link>
                         <p className='post-info'> 지역 : {post.area} </p>
                         <p className='post-info'>
@@ -67,9 +67,9 @@ const MainWriteList = () => {
                             카테고리 : {post.category}{' '}
                         </p>
                         <p className='post-info'> 작성일 : {post.date} </p>
+                            <img src={post.profileImage} alt='프로필' />
                         <p className='post-info'>
                             작성자 : {post.nickname}
-                            <img src={post.profileImage} alt='프로필' />
                         </p>
                         <p className='post-content'> {post.content.slice(0,5)+'...'} </p>
                     </li>
